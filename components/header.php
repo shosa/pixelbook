791451e2d0c9b00c726ca588d47aff6d55471abb
Stefano Solidoro <kishosa@me.com>
2024-10-01 17:41:36 +0200

    piccole modifiche

--- components/header.php
+++ components/header.php
@@ -7,8 +7,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PixelBook</title>
         <link rel="stylesheet" href="components/style.css">
-        <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
         <link href="vendor/fortawesome/font-awesome/css/all.min.css" rel="stylesheet">
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet">
     </head>
 
 
@@ -23,27 +26,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="categories.php">Categorie</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="bookings.php">Prenotazioni</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Altro
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Opzione 1</a></li>
-                            <li><a class="dropdown-item" href="#">Opzione 2</a></li>
-                            <li><a class="dropdown-item" href="#">Opzione 3</a></li>
-                        </ul>
-                    </li>
-                </ul>
+
+
             </div>
         </nav>
--- index.php
+++ index.php
@@ -9,50 +9,13 @@ $categorie = $stmt->fetchAll();
 ?>
 <style>
     /* styles.css */
-
-    /* Stile di base */
-    body {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-        background-color: #f8f8f8;
-        color: #333;
-    }
-
-    .container {
-        padding: 20px;
-    }
-
-    /* Stile per i banner delle categorie */
-    .category-link {
-        text-decoration: none;
-        display: block;
-    }
-
-    .category-banner {
-        position: relative;
-        background-size: cover;
-        background-position: center;
-        border-radius: 12px;
-        overflow: hidden;
-        height: 200px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
-
-    .category-title {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background: rgba(0, 0, 0, 0.5);
-        color: #fff;
-        text-align: center;
-        padding: 10px;
-        border-top-left-radius: 12px;
-        border-top-right-radius: 12px;
-        font-size: 1.5em;
-    }
 </style>
 <div class="container">
-    <h1 class="text-center font-weight-bold text-indigo">Book the Best</h1>
+    <h6 class="h2 text-center font-weight-bold text-dark">Book the <span class="h1 text-indigo font-weight-bold">
+            Best</span></h6>
+    <h6 class=" h3 text-center font-weight-bold text-dark">in <span class="h1 text-indigo font-weight-bold">3</span>
+        simple
+        steps.</h6>
     <div class="row ">
         <?php foreach ($categorie as $categoria): ?>
             <div class="col-md-4 mb-4">
